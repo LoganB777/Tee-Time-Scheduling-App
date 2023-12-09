ba8b6e161ebb3368dc930227a3603172c8ba7720
LoganB777 <148018702+LoganB777@users.noreply.github.com>
2023-12-08 19:44:10 -0600

    Add files via upload

---
+++ Presentation/Program.cs
@@ -0,0 +1,15 @@
+﻿using LogicLayer;
+using System;
+
+namespace Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Tee Time Scheduler!");
+            Helper helper = new Helper();
+            helper.MainMenu();
+        }
+    }
+}
